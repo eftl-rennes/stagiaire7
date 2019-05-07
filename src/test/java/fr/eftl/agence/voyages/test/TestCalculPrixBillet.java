925202042dda2cb9c2110e2fc96d4dc549a83c23
Ewan <eftl2019@gmail.com>
2019-05-07 14:24:04 +0200

    integration test

--- src/test/java/fr/eftl/agence/voyages/test/TestCalculPrixBillet.java
+++ src/test/java/fr/eftl/agence/voyages/test/TestCalculPrixBillet.java
@@ -20,7 +20,46 @@ public class TestCalculPrixBillet {
 		
 		prixBillet.setCalculPrixUnitaire(calculPrixUnitaire);
 		
-		Integer prixObtenu = prixBillet.calculer("Paris", "Londres", "Eco");
+		Integer prixObtenu = prixBillet.calculer("Paris", "Londres", "eco");
+		Assert.assertEquals(200, prixObtenu,0);
+	}
+	
+	@Test
+	public void prixBilletClassePremiere() {
+		CalculPrixBillet prixBillet = new CalculPrixBillet();
+		//CalculPrixUnitaire calculPrixUnitaire = new CalculPrixUnitaire();
+		CalculPrixUnitaire calculPrixUnitaire= Mockito.mock(CalculPrixUnitaire.class);
+		Mockito.when(calculPrixUnitaire.generer()).thenReturn(200);
+		
+		prixBillet.setCalculPrixUnitaire(calculPrixUnitaire);
+		
+		Integer prixObtenu = prixBillet.calculer("Paris", "Londres", "premiere");
+		Assert.assertEquals(400, prixObtenu,0);
+	}
+	
+	@Test
+	public void prixBilletClasseBusiness() {
+		CalculPrixBillet prixBillet = new CalculPrixBillet();
+		//CalculPrixUnitaire calculPrixUnitaire = new CalculPrixUnitaire();
+		CalculPrixUnitaire calculPrixUnitaire= Mockito.mock(CalculPrixUnitaire.class);
+		Mockito.when(calculPrixUnitaire.generer()).thenReturn(200);
+		
+		prixBillet.setCalculPrixUnitaire(calculPrixUnitaire);
+		
+		Integer prixObtenu = prixBillet.calculer("Paris", "Londres", "business");
+		Assert.assertEquals(700, prixObtenu,0);
+	}
+	
+	@Test
+	public void prixBilletClasseDefault() {
+		CalculPrixBillet prixBillet = new CalculPrixBillet();
+		//CalculPrixUnitaire calculPrixUnitaire = new CalculPrixUnitaire();
+		CalculPrixUnitaire calculPrixUnitaire= Mockito.mock(CalculPrixUnitaire.class);
+		Mockito.when(calculPrixUnitaire.generer()).thenReturn(200);
+		
+		prixBillet.setCalculPrixUnitaire(calculPrixUnitaire);
+		
+		Integer prixObtenu = prixBillet.calculer("Paris", "Londres", "");
 		Assert.assertEquals(200, prixObtenu,0);
 	}
 	
